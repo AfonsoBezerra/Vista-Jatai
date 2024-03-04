a3d620580bca45556722a0e520d4b607b7d613f3
Afonso <84323490+AfonsoBezerra@users.noreply.github.com>
2024-03-04 15:49:16 -0300

    Update style.ts

--- src/components/hero/style.ts
+++ src/components/hero/style.ts
@@ -2,13 +2,12 @@ import styled from 'styled-components';
 
 export const HeroStyle = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 85vh;
   background-color: black;
 
   img {
     width: 100%;
     height: 100%;
-    object-fit: cover;
     @media (max-width: 1023px) {
       width: 100%;
       object-fit: cover;
